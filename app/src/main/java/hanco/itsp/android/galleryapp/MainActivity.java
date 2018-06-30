e0c6d840b6c585506fb93c6642381a1796c4f3a1
Anubhav Agarwal <agarwalanubhav.2011@gmail.com>
2018-06-30 18:44:10 +0530

    Initial commit

---
+++ app/src/main/java/hanco/itsp/android/galleryapp/MainActivity.java
@@ -0,0 +1,50 @@
+package hanco.itsp.android.galleryapp;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static int RESULT_LOAD = 1;
+    String img_Decodable_Str;
+    final int PICK_IMAGE=100;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView=findViewById(R.id.imgView1);
+        Button button=findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pickimg=new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                startActivityForResult(pickimg,PICK_IMAGE);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(resultCode==RESULT_OK && requestCode== PICK_IMAGE){
+            Uri imageUri=data.getData();
+            imageView.setImageURI(imageUri);
+            Image image=imageView.getBackground();
+
+        }
+
+    }
+
+}
